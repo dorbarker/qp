2600ae1cd6e18f7459f3749f2d9c35c0e3f8d90f
Dillon Barker <dor.barker@gmail.com>
2016-06-07 21:13:44 -0600

    Basic graph methods

--- GraphController.py
+++ GraphController.py
@@ -1,10 +1,32 @@
 import networkx as nx
 import numpy as np
+from collections import defaultdict
 
 class Pangenome(object):
 
     def __init__(self):
 
-        g = nx.Graph()
+       clusters = defaultdict(nx.Graph)
 
+    def add_to_cluster(self, cluster: str, gene1: str, gene2: str,
+                       distance: float):
 
+        self.clusters[cluster].add_edge(gene1, gene2, weight=distance)
+
+    def join_clusters(self, cluster1: str, cluster2: str,
+                      gene1: str, gene2: str, middle_gene: str,
+                      distance1: float, distance2: float):
+
+        G = self.clusters.pop(cluster2)
+        self.clusters[cluster1].add_edges_from(self.clusters[cluster2].edges())
+        self.add_to_cluster(cluster1, gene1, middle_gene, distance1)
+        self.add_to_cluster(cluster1, middle_gene, gene2, distance2)
+
+    def add_founder(self, gene):
+        """Creates the founding node of a new cluster graph.
+
+        The graph is named after the founding gene, and is *not*
+        updated when centre changes.
+        """
+
+        self.clusters[gene].add_node(gene)
